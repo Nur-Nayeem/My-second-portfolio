bb018107bc256e7b00c44a50576c157e4adecd84
nur-nayeem <nurnayem768@gmail.com>
2025-11-07 23:57:00 +0600

    initialize the portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen gradient-bg">
+      <h2>Hello</h2>
+    </div>
+  );
+};
+
+export default App;
